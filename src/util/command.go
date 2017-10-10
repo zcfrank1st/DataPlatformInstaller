203f99e1a18ddac85d553a28237bbb9f2ef83fbe
zcfrank1st <zcfrank1st@sina.com>
2017-10-10 12:02:56 +0800

    add commands

---
+++ src/util/command.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+    "os/exec"
+    "os"
+)
+
+const WGET_DIR = "/Users/zcfrank1st/Desktop/"
+
+func commandRun (name string, arg ...string) {
+    cmd := exec.Command(name, arg...)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    cmd.Run()
+}
+
+func Wget(url string) {
+    commandRun("wget", "-P", WGET_DIR, url)
+}
+
+func Scp(hosts string) {
+    // todo scp
+    commandRun("scp", "")
+}
