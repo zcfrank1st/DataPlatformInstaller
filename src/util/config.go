8b2b773837b73eb15594440b23b03f605ea35de2
zcfrank1st <zcfrank1st@sina.com>
2017-10-19 09:42:51 +0800

    role config

--- src/util/config.go
+++ src/util/config.go
@@ -1,12 +1,12 @@
 package util
 
 // todo cluster global variables
-
 var (
-
+    RoleMapper map[string]string  // {"NameNode": "192.168.33.1", }
 )
 
 
 func SaveConfigToLocal() {
     //todo write config to let ui and monitor use
+
 }
---
+++ src/util/tool.go
@@ -0,0 +1,6 @@
+package util
+
+
+func CheckDirs() {
+    // todo
+}
